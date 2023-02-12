aa27e7c30d049246d26533d650614487a9c476c9
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2023-02-12 19:42:01 -0300

    att

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ export const Contact = () => {
   const { t } = useTranslation();
 
   return (
-    <div id="contact" className="dark:bg-primary-900 text-white flex flex-col items-center gap-24 pt-20 pb-10 z-10 max-sm:gap-16 max-sm:pt-10 max-sm:pb-20 ">
+    <div id="contact" className="dark:bg-primary-900 text-white flex flex-col items-center gap-24 pt-20 pb-10 z-10 max-sm:gap-16 max-sm:pt-10 max-sm:pb-5 ">
       <Fade delay={100} duration={1500} className="w-4/6 max-w-5xl flex flex-col items-center max-sm:w-4/5">
         <h1 className="text-4xl font-bold dark:text-white text-primary-900 max-sm:text-3xl">{t("contact-title")}</h1>
         <div className="text-black flex gap-20 max-lg:flex-col">
